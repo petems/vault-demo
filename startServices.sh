0fecb76da937dbe13f07ebd6018f4b289093f956
petems <p.morsou@gmail.com>
2020-08-03 13:32:32 +0100

    Update to latest version

--- startServices.sh
+++ startServices.sh
@@ -2,8 +2,8 @@
 eval $(minikube docker-env)
 
 echo "Pulling images"
-docker pull vault:0.7.3
-docker pull consul:0.8.5
+docker pull vault:1.4.3
+docker pull consul:1.8.0
 docker pull mysql:latest
 
 kubectl create -f k8s-config/consul.yaml
